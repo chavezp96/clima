ca7f1cf35894729d616ec49ffd741bf09ff7b2c5
chavezp96 <chavezp96@outlook.com>
2018-04-14 15:36:01 -0500

    Agregado Guzzle, Agregado get_clima.php

---
+++ get_clima.php
@@ -0,0 +1,13 @@
+<?php
+    require 'vendor/autoload.php';
+
+    use GuzzleHttp\Client;
+    use GuzzleHttp\Psr7\Request;
+
+    $client = new GuzzleHttp\Client(['base_uri' => 'http://api.openweathermap.org/data/2.5/']);
+    $response = $client->request('GET', 'weather?id=3530597&appid=7d43276e33430314aa97ca382ebf70f3');
+
+    header("Content-type: application/json");
+    echo $response->getBody();
+
+?>
